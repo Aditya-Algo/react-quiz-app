16f76c0d5f41ef27103451089fb4be98ba054970
Aditya Dey <deyaditya2004@gmail.com>
2025-02-08 13:55:41 +0530

    Routing Complete

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,5 +1,6 @@
 import classes from '../styles/ProgressBar.module.css';
 import Button from './Button';
+import {Link} from 'react-router-dom';
 
 export default function Answers() {
   return (
@@ -13,12 +14,12 @@ export default function Answers() {
               <div className={classes.progress} style={{width: "20%"}}></div>
             </div>
           </div>
-          <a href="result.html">
+          <Link to="/result">
             <Button className={classes.next}>
             <span>Next Question</span>
             <span className="material-icons-outlined"> arrow_forward </span>
             </Button>
-          </a>
+          </Link>
         </div>
     
   );
